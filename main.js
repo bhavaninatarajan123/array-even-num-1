200b410a1527a3a57f2a86450a6e61d077eaeb5a
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-20 15:41:37 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,9 @@
+var a=[2,3,6,12,7,8,5];
+var b=[];
+document.write("Given array is [2,3,6,12,7,8,5]");
+for(let i=0; i<a.length; i++){
+							  if (a[i]%2==0){
+										b.push(a[i]);
+										}
+							}
+							console.log(b);
